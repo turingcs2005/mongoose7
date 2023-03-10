3bc04a9c8a69b16178420a3a9c3050e7c01dd299
Rui Wang <rui.wang.624@gmail.com>
2023-03-09 20:54:53 -0500

    minor change

--- routes/student.route.js
+++ routes/student.route.js
@@ -5,18 +5,25 @@ const Student = require('../models/student.model');
 
 // get student by name
 router.get('/getStudentByName/:name', async (req, res) => {
-    res.json(
-        await Student.findOne({
+    try {
+        const data = await Student.findOne({
             name: req.params.name
-        })
-    );
+        });
+        res.json(data);
+    } catch (e) {
+        console.log('Failed to query MongoDB database: ', e);
+    }
 });
 
 // post a new student
 router.post('/addStudent', async (req, res) => {
-    res.json(
-        await Student.create(req.body)
-    );
+    try {
+        const data = await Student.create(req.body);
+        res.json(data);
+        console.log('Data successfully saved to MongoDB database.');
+    } catch (e) {
+        console.log('Failed to save data to MongoDB database: ', e);
+    }
 });
 
 module.exports = router;
